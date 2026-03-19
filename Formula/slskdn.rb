b0513a67cecd72c8d060a12b718597ef2a5e9d4a
slskdn CI <slskdn@proton.me>
2026-03-19 01:10:54 +0000

    Update slskdn to 0.24.5-slskdn.64

--- Formula/slskdn.rb
+++ Formula/slskdn.rb
@@ -2,22 +2,22 @@ class Slskdn < Formula
   desc "Batteries-included Soulseek web client"
   homepage "https://github.com/snapetech/slskdn"
   license "AGPL-3.0-or-later"
-  version "0.24.5-slskdn.62"
-  url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.62/slskdn-main-linux-x64.zip"
-  sha256 "d48325423324c405415deb83685e26aebb66ddf91daa8d87a5841742b3d5671e"
+  version "0.24.5-slskdn.64"
+  url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.64/slskdn-main-linux-x64.zip"
+  sha256 "3981276ef4261c11bd1b42084d1ddeb3801af0aa1f52610c0c18f21bc5ab5fa2"
   on_macos do
     on_arm do
-      url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.62/slskdn-main-osx-arm64.zip"
-      sha256 "f4356d4021150898758bec37bd56cc525b5b469aec4b22d3e2eb44e2ba2ac29d"
+      url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.64/slskdn-main-osx-arm64.zip"
+      sha256 "9f13785a88ea9862aaa978eebcfc9e8f40fd3bbd91b8265325e0d9c2e30bff2b"
     end
     on_intel do
-      url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.62/slskdn-main-osx-x64.zip"
-      sha256 "6a8279799a676519bed8c2c98733593210dbe3acef0ae0177b9679d2ce2c9b8e"
+      url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.64/slskdn-main-osx-x64.zip"
+      sha256 "be2c44c2c5ead41a4f6e4c865e9c513433f5e1af5bc41509321b203acfb5dbcc"
     end
   end
   on_linux do
-    url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.62/slskdn-main-linux-x64.zip"
-    sha256 "d48325423324c405415deb83685e26aebb66ddf91daa8d87a5841742b3d5671e"
+    url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.64/slskdn-main-linux-x64.zip"
+    sha256 "3981276ef4261c11bd1b42084d1ddeb3801af0aa1f52610c0c18f21bc5ab5fa2"
   end
   conflicts_with "slskdn-dev", because: "both install slskd binaries"
   def install

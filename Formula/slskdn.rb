b9bfcb45afa2f2df124a0ac467f61cef7fc687fd
slskdn CI <slskdn@proton.me>
2026-03-29 07:16:50 +0000

    Update slskdn to 0.24.5-slskdn.108

--- Formula/slskdn.rb
+++ Formula/slskdn.rb
@@ -2,22 +2,22 @@ class Slskdn < Formula
   desc "Batteries-included Soulseek web client"
   homepage "https://github.com/snapetech/slskdn"
   license "AGPL-3.0-or-later"
-  version "0.24.5-slskdn.107"
-  url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.107/slskdn-main-linux-x64.zip"
-  sha256 "d7c3453769b38544fc12823c09cdde1f33d87235867d741234bd21eb3c236e23"
+  version "0.24.5-slskdn.108"
+  url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.108/slskdn-main-linux-x64.zip"
+  sha256 "ac38f506435c15e59f19401672a88d8b501296544cbf04ab9811dbb805624b33"
   on_macos do
     on_arm do
-      url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.107/slskdn-main-osx-arm64.zip"
-      sha256 "6b1ec71b82b94f4d50f5e2a26a590c20a84b093acaba7d2ccef52e54a086a638"
+      url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.108/slskdn-main-osx-arm64.zip"
+      sha256 "d214db148dc2bf0f49a151ccd17e13dd8fba0ea9486fd53ba12781064bb9ad5b"
     end
     on_intel do
-      url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.107/slskdn-main-osx-x64.zip"
-      sha256 "176238491f5898e9c2164f02e618dcffe3946cb3da35c90c8654da1051a91136"
+      url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.108/slskdn-main-osx-x64.zip"
+      sha256 "38c9687f99743d167f5e51aced499072c955621b46277611ed269bcab19d2360"
     end
   end
   on_linux do
-    url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.107/slskdn-main-linux-x64.zip"
-    sha256 "d7c3453769b38544fc12823c09cdde1f33d87235867d741234bd21eb3c236e23"
+    url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.108/slskdn-main-linux-x64.zip"
+    sha256 "ac38f506435c15e59f19401672a88d8b501296544cbf04ab9811dbb805624b33"
   end
   conflicts_with "slskdn-dev", because: "both install slskd binaries"
   def install

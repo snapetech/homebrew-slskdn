ce27d700a325a61d5f292b48dc66a34a42aa531a
slskdn CI <slskdn@proton.me>
2026-04-18 17:29:04 +0000

    Update slskdn to 0.24.5-slskdn.147

--- Formula/slskdn.rb
+++ Formula/slskdn.rb
@@ -2,22 +2,22 @@ class Slskdn < Formula
   desc "Batteries-included Soulseek web client"
   homepage "https://github.com/snapetech/slskdn"
   license "AGPL-3.0-or-later"
-  version "0.24.5-slskdn.146"
-  url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.146/slskdn-main-linux-glibc-x64.zip"
-  sha256 "f46e163183fe111ced5f2b3c2ab063f0ecdb386d655f831a5e350525dbf5b9b7"
+  version "0.24.5-slskdn.147"
+  url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.147/slskdn-main-linux-glibc-x64.zip"
+  sha256 "d32cdabf7af35792bc989bc904c1753775e1e148f2072d4ec052cca1736d0aaa"
   on_macos do
     on_arm do
-      url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.146/slskdn-main-osx-arm64.zip"
-      sha256 "533499f11c5a4af9788b3342ccfdb5a87f5101f6ee819e0e21dc106375035907"
+      url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.147/slskdn-main-osx-arm64.zip"
+      sha256 "86b0a9f3b4b128a511a425e1e3c144d390ccefba7173185864a982382e2ce9e6"
     end
     on_intel do
-      url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.146/slskdn-main-osx-x64.zip"
-      sha256 "e1c13d3b67f08eec8afb1e64823132c35626084bdcb28c2399b004fa2660d38d"
+      url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.147/slskdn-main-osx-x64.zip"
+      sha256 "f94ead8b739d724fa3e113ba55418f02632bd0acaa1df603da5d9a9f9062305c"
     end
   end
   on_linux do
-    url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.146/slskdn-main-linux-glibc-x64.zip"
-    sha256 "f46e163183fe111ced5f2b3c2ab063f0ecdb386d655f831a5e350525dbf5b9b7"
+    url "https://github.com/snapetech/slskdn/releases/download/0.24.5-slskdn.147/slskdn-main-linux-glibc-x64.zip"
+    sha256 "d32cdabf7af35792bc989bc904c1753775e1e148f2072d4ec052cca1736d0aaa"
   end
   conflicts_with "slskdn-dev", because: "both install slskd binaries"
   def install

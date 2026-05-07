c397ca00c856c2c20cd523af8ac830b5fc8a1db9
slskdn CI <slskdn@proton.me>
2026-05-07 18:40:15 +0000

    Update slskdn to 2026050712-slskdn.232

--- Formula/slskdn.rb
+++ Formula/slskdn.rb
@@ -2,22 +2,22 @@ class Slskdn < Formula
   desc "Unofficial slskd fork with batteries-included Soulseek features"
   homepage "https://github.com/snapetech/slskdn"
   license "AGPL-3.0-or-later"
-  version "2026050711-slskdn.231"
-  url "https://github.com/snapetech/slskdN/releases/download/2026050711-slskdn.231/slskdn-main-linux-glibc-x64.zip"
-  sha256 "a62f5e5596a7a11db49494d7b577360b9204487825aad680778c8620e4bb7717"
+  version "2026050712-slskdn.232"
+  url "https://github.com/snapetech/slskdN/releases/download/2026050712-slskdn.232/slskdn-main-linux-glibc-x64.zip"
+  sha256 "019ea295fc89cdc7db1c86de9faf1baa01d1ba502ed73f12fe73360b823ff671"
   on_macos do
     on_arm do
-      url "https://github.com/snapetech/slskdN/releases/download/2026050711-slskdn.231/slskdn-main-osx-arm64.zip"
-      sha256 "dfc1764cff0d4a642a9f49d48b9e98f50d2572d65f800e69c65e3538f315c761"
+      url "https://github.com/snapetech/slskdN/releases/download/2026050712-slskdn.232/slskdn-main-osx-arm64.zip"
+      sha256 "6d68ba2a299d19fac8bc48eb3f9a2cbdc3180eafafade36decfc1ccf5bee7321"
     end
     on_intel do
-      url "https://github.com/snapetech/slskdN/releases/download/2026050711-slskdn.231/slskdn-main-osx-x64.zip"
-      sha256 "b742187cd91d796087052e216cd841af8042c1a429d250b5923936d056311c79"
+      url "https://github.com/snapetech/slskdN/releases/download/2026050712-slskdn.232/slskdn-main-osx-x64.zip"
+      sha256 "9e70a4fcad7d80c1d561e5422f53b5de6c11c06a0b8312d89d107c82ff01dedb"
     end
   end
   on_linux do
-    url "https://github.com/snapetech/slskdN/releases/download/2026050711-slskdn.231/slskdn-main-linux-glibc-x64.zip"
-    sha256 "a62f5e5596a7a11db49494d7b577360b9204487825aad680778c8620e4bb7717"
+    url "https://github.com/snapetech/slskdN/releases/download/2026050712-slskdn.232/slskdn-main-linux-glibc-x64.zip"
+    sha256 "019ea295fc89cdc7db1c86de9faf1baa01d1ba502ed73f12fe73360b823ff671"
   end
   def install
     libexec.install Dir["*"]

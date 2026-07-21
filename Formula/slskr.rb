b9ea55985d01b3df8d55673ecb49d36078a92d2c
slskr CI <slskr@proton.me>
2026-07-21 04:29:33 +0000

    Update slskr to v0.2.27

--- Formula/slskr.rb
+++ Formula/slskr.rb
@@ -2,26 +2,26 @@ class Slskr < Formula
   desc "Rust Soulseek daemon with bundled Web UI"
   homepage "https://github.com/snapetech/slskr"
   license "AGPL-3.0-only"
-  version "0.2.26"
+  version "0.2.27"
 
   on_macos do
     on_arm do
-      url "https://github.com/snapetech/slskr/releases/download/release-v0.2.26/slskr-v0.2.26-aarch64-apple-darwin.tar.gz"
-      sha256 "4d368b0011a074d1f91104e4e2aaa9f87c0908ce19a097c9b7e4ed63307345f9"
+      url "https://github.com/snapetech/slskr/releases/download/release-v0.2.27/slskr-v0.2.27-aarch64-apple-darwin.tar.gz"
+      sha256 "e2fe43d22e66ed6b11b2dc0620dbec98ab6771df1f112ce947475261116e49d6"
     end
     on_intel do
-      url "https://github.com/snapetech/slskr/releases/download/release-v0.2.26/slskr-v0.2.26-x86_64-apple-darwin.tar.gz"
-      sha256 "af7fdf00225ef57235423e943cc769945c60eaf1c813e87b78ff47758f894681"
+      url "https://github.com/snapetech/slskr/releases/download/release-v0.2.27/slskr-v0.2.27-x86_64-apple-darwin.tar.gz"
+      sha256 "19224c0aeab0eefc560d3d3776a389fc546aa0fcfe993621f83f88c7dc1f20d7"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/snapetech/slskr/releases/download/release-v0.2.26/slskr-v0.2.26-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "9497ad4d018e64d6c827b3ac0c9e22f6211085c9b9c95d3ee1ee1e568283c4fe"
+      url "https://github.com/snapetech/slskr/releases/download/release-v0.2.27/slskr-v0.2.27-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "4d652669944f156610237544e8708bc1ca5926297444d6431128dfd822af0576"
     else
-      url "https://github.com/snapetech/slskr/releases/download/release-v0.2.26/slskr-v0.2.26-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "c226fc98b95e930f42e54af32821d53453f8df6b32d0fb8d2498463ff53e6d5d"
+      url "https://github.com/snapetech/slskr/releases/download/release-v0.2.27/slskr-v0.2.27-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "19a9af493536bb91be2b1d7c0ff288d86aa593bf101c4215d2b650266d3de335"
     end
   end
 

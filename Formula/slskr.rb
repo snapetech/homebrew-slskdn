c70246c69c70a7a6ae3fc46967d3fbda6e711710
slskr CI <slskr@proton.me>
2026-08-22 14:50:33 +0000

    Update slskr to v0.2.32

--- Formula/slskr.rb
+++ Formula/slskr.rb
@@ -7,21 +7,21 @@ class Slskr < Formula
   on_macos do
     on_arm do
       url "https://github.com/snapetech/slskr/releases/download/release-v0.2.32/slskr-v0.2.32-aarch64-apple-darwin.tar.gz"
-      sha256 "e2c5d3cb03f63c8175da21683556973ddfc92c9b74e1474f538ccd88894741bd"
+      sha256 "35552b7a9d2a956c92c4d61a3c5afbd554923e579d9dbe006d2614556bbe77a8"
     end
     on_intel do
       url "https://github.com/snapetech/slskr/releases/download/release-v0.2.32/slskr-v0.2.32-x86_64-apple-darwin.tar.gz"
-      sha256 "58943667fe232756f55024d2829eb7932830618e8667b205ced6cde39f2d71ec"
+      sha256 "ae66c577f79085debab6c79d27e78d0253ae721ce2530245346052ea4b792743"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/snapetech/slskr/releases/download/release-v0.2.32/slskr-v0.2.32-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "77c5de40da434626b8d19a7ae3a272d29c2cd7290038014cc7f8a35043260417"
+      sha256 "30187b45b8606fdb4a018174cdf510238adedc23058be53a7217f25ae1fbf651"
     else
       url "https://github.com/snapetech/slskr/releases/download/release-v0.2.32/slskr-v0.2.32-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "ed53cfdb10883a7aa96d3e70a4b3adc1c6ffdde509968bb9435744ae88780a4e"
+      sha256 "0f581f93655725f66a302891947760d84973461322e1dc3d523a5dbb9edc325f"
     end
   end
 
